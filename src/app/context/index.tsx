d3577e3889c836a0906d614dfa929d7d4e898ff3
AVBlana <71121584+AVBlana@users.noreply.github.com>
2023-11-24 21:20:20 +0200

    error fixed ts

--- src/app/context/index.tsx
+++ src/app/context/index.tsx
@@ -1,9 +1,13 @@
 "use client";
 
+const IS_DEVELOPMENT = process.env.NODE_ENV;
+
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000",
+  uri: IS_DEVELOPMENT
+    ? "http://localhost:4000/graphql"
+    : "https://locals-only-da073e224bd3.herokuapp.com/graphql",
   cache: new InMemoryCache(),
 });
 
